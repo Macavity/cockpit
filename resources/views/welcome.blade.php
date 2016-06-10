61ebd5361965e21394934c80ee2af3b3f791c6ab
Alexander Pape <a.pape@paneon.de>
2016-06-10 10:04:03 +0200

    Base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,42 +3,23 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+      <!-- 1. Load libraries -->
+      <!-- Polyfill(s) for older browsers -->
+      <script src="/core-js/client/shim.min.js"></script>
+      <script src="/zone.js/dist/zone.js"></script>
+      <script src="/reflect-metadata/Reflect.js"></script>
+      <script src="/systemjs/dist/system.src.js"></script>
+      <!-- 2. Configure SystemJS -->
+      <script src="/systemjs.config.js"></script>
+      <script>
+        System.import('app').catch(function(err){ console.error(err); });
+      </script>
     </head>
     <body>
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <app>Lädt...</app>
             </div>
         </div>
     </body>
