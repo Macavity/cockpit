6455d9201ea9196e0bdbb8ddea52147b4710b8dc
Alexander Pape <alexander.pape@holzhofer-consulting.de>
2016-09-02 16:52:02 +0200

    Add dependant components into the new AppModule

--- resources/assets/typescript/app/app.module.ts
+++ resources/assets/typescript/app/app.module.ts
@@ -8,25 +8,39 @@ import { HttpModule } from '@angular/http';
  */
 import { ENV_PROVIDERS } from '../environment';
 import { APP_ROUTING } from './app.routes';
+
 // App is our top level component
 import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
-import { ApiService } from "./Services/api.service";
-import { UserService } from "./Services/user.service";
-import { LoginComponent } from "./modules/login.component";
+import { LoginComponent } from "./modules/login/login.component";
 import { DashboardComponent } from "./modules/dashboard/dashboard.component";
+import { NoContentComponent } from "./modules/no-content/no-content";
 
-
+import { ApiService } from "./services/api.service";
+import { AuthService } from "./services/auth.service";
+import { UserService } from "./services/user.service";
+import { NavigationService } from "./modules/navigation/navigation.service";
+import { LanguageService } from "./services/language/language.service";
+import { HomeComponent } from "./modules/home/home.component";
+import { HeaderComponent } from "./components/header.component";
+import { AuthHttp, provideAuth } from "angular2-jwt";
+import { AuthGuard } from "./services/auth.guard";
 
 // Application wide providers
 const APP_PROVIDERS = [
     ...APP_RESOLVER_PROVIDERS,
 
+    // AuthHttp
+    AuthHttp,
+    provideAuth(AuthService.getAuthConfig()),
+
+    // App Services
     ApiService,
-    //LanguageService,
-    //NavigationService,
+    LanguageService,
+    NavigationService,
     UserService,
     AuthService,
+    AuthGuard,
 ];
 
 /**
@@ -35,11 +49,14 @@ const APP_PROVIDERS = [
 @NgModule({
     bootstrap: [ AppComponent ],
     declarations: [
+        // App Components
         AppComponent,
         LoginComponent,
         DashboardComponent,
 
         NoContentComponent,
+        HomeComponent,
+        HeaderComponent
     ],
     imports: [
         // import Angular's modules
@@ -49,10 +66,6 @@ const APP_PROVIDERS = [
 
         // Our Modules
         APP_ROUTING,
-        CourseModule,
-        UserModule,
-        BookingOverviewModule,
-        ServicesOverviewModule
     ],
     providers: [ // expose our Services and Providers into Angular's dependency injection
         ENV_PROVIDERS,
