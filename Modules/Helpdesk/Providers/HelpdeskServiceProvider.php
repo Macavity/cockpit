9ce99dfe909f6348e00066d60acaf115f196a314
Alexander Pape <alexander.pape@holzhofer-consulting.de>
2016-10-19 17:34:39 +0200

    Create modules for Dashboard and Helpdesk

---
+++ Modules/Helpdesk/Providers/HelpdeskServiceProvider.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Modules\Helpdesk\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class HelpdeskServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerTranslations();
+        $this->registerConfig();
+        $this->registerViews();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            __DIR__.'/../Config/config.php' => config_path('helpdesk.php'),
+        ]);
+        $this->mergeConfigFrom(
+            __DIR__.'/../Config/config.php', 'helpdesk'
+        );
+    }
+
+    /**
+     * Register views.
+     *
+     * @return void
+     */
+    public function registerViews()
+    {
+        $viewPath = base_path('resources/views/modules/helpdesk');
+
+        $sourcePath = __DIR__.'/../Resources/views';
+
+        $this->publishes([
+            $sourcePath => $viewPath
+        ]);
+
+        $this->loadViewsFrom(array_merge(array_map(function ($path) {
+            return $path . '/modules/helpdesk';
+        }, \Config::get('view.paths')), [$sourcePath]), 'helpdesk');
+    }
+
+    /**
+     * Register translations.
+     *
+     * @return void
+     */
+    public function registerTranslations()
+    {
+        $langPath = base_path('resources/lang/modules/helpdesk');
+
+        if (is_dir($langPath)) {
+            $this->loadTranslationsFrom($langPath, 'helpdesk');
+        } else {
+            $this->loadTranslationsFrom(__DIR__ .'/../Resources/lang', 'helpdesk');
+        }
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
